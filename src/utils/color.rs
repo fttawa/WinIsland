2b2d0adeac2d0b9430195b91f3dee05b56637a6b
Eatgrapes <Eatgrapesem232@qq.com>
2026-03-01 14:47:28 +0800

    Del unused color

--- src/utils/color.rs
+++ src/utils/color.rs
@@ -4,7 +4,6 @@ pub const COLOR_BG: Color = Color::from_rgb(28, 28, 30);
 pub const COLOR_CARD: Color = Color::from_rgb(44, 44, 46);
 pub const COLOR_CARD_HIGHLIGHT: Color = Color::from_rgb(63, 63, 66);
 pub const COLOR_ACCENT: Color = Color::from_rgb(10, 132, 255);
-pub const COLOR_ACCENT_PINK: Color = Color::from_rgb(255, 45, 85);
 pub const COLOR_TEXT_PRI: Color = Color::WHITE;
 pub const COLOR_TEXT_SEC: Color = Color::from_rgb(142, 142, 147);
 pub const COLOR_DANGER: Color = Color::from_rgb(255, 69, 58);
